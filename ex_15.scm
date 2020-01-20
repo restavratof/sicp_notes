2afcad7c1448d04546e3309225655c11f87a2f8a
restavratof <d.faschenko@gmail.com>
2020-01-20 19:04:54 +0300

    блочная структура внедрена

--- ex_15.scm
+++ ex_15.scm
@@ -1,30 +1,19 @@
 (define (square x) (* x x) )
 
-(define (good-enough? guess x)
-    (< (abs (- (square guess) x)) 0.001))
-
-(define (average x y)
-    (/ (+ x y) 2))
-
-(define (improve guess x)
-    (average guess (/ x guess)))
-
-(define (sqrt-iter guess x)
-    (if (good-enough? guess x)
-        guess
-        (sqrt-iter (improve guess x)
-            x)))
-
-(define (good-enough? guess x)
-    (< (abs (- (square guess) x)) 0.001))
-
 (define (sqrt x)
-    (sqrt-iter 1.0 x))
+    (define (good-enough? guess)
+        (< (abs (- (square guess) x)) 0.001))
+    (define (average x y)
+        (/ (+ x y) 2))
+    (define (improve guess)
+        (average guess (/ x guess)))
+    (define (sqrt-iter guess)
+        (if (good-enough? guess)
+            guess
+            (sqrt-iter (improve guess))))
+    (sqrt-iter 1.0))
 
 (sqrt 9)
-
 (sqrt (+ 100 37))
-
 (sqrt (+ (sqrt 2) (sqrt 3)))
-
 (square (sqrt 1000))
--- ex_18.scm
+++ ex_18.scm
@@ -1,38 +1,21 @@
-;--------------------------------------------------------
-; Square calculation
-;--------------------------------------------------------
 (define (square x) (* x x) )
 
-;--------------------------------------------------------
-; Cube calculation
-;--------------------------------------------------------
 (define (cube x) (* x x x))
 
-;--------------------------------------------------------
-; Check that result within required accuracy
-;--------------------------------------------------------
-(define (good-enough? guess x)
-    (< (abs (- (cube guess) x)) 0.001))
-
-;--------------------------------------------------------
-(define (average x y)
-    (/ (+ x y) 3))
-
-(define (improve guess x)
-    (average (/ x (square guess)) (* 2 guess) ))
-
-;--------------------------------------------------------
-; Sqrt calculation
-;--------------------------------------------------------
-(define (cubert-iter guess x)
-    (if (good-enough? guess x)
-        guess
-        (cubert-iter (improve guess x)
-            x)))
-
 (define (cubert x)
-    (cubert-iter 1.0 x))
+    (define (good-enough? guess)
+        (< (abs (- (cube guess) x)) 0.001))
+    (define (average x y)
+        (/ (+ x y) 3))
+    (define (improve guess)
+        (average (/ x (square guess)) (* 2 guess) ))
+    (define (cubert-iter guess)
+        (if (good-enough? guess)
+            guess
+            (cubert-iter (improve guess))))
+    (cubert-iter 1.0))
 
 
 
-(cubert 81)
+(cubert 27)
+(cubert 205379)
